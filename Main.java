6fd48c0ba8042e2435fa4fa4968c12a1d0ca6ada
Oscar <oscarisback1908@gmail.com>
2024-12-03 22:46:23 -0500

    day 2

--- Main.java
+++ Main.java
@@ -17,7 +17,15 @@ public class Main{
         System.out.println("point: " + point);
         System.out.println(Point.distance(a, b));
         System.out.println(abDistance); 
-
-        // Left off at Practice angles
+        //challenge 1
+        Point[] points = {new Point(4,1), new Point(5,7), new Point(10,8)};
+        Point centerMass = Point.centerOfMass(points);
+        System.out.println(centerMass);
+        System.out.println(points.length);
+        //challenge 2
+        Point testangle = new Point(-2,-8);
+        double tested = testangle.angle();
+        System.out.println(tested);
+        // Left off at Polymorphism
     }
 }
--- Point.java
+++ Point.java
@@ -1,6 +1,6 @@
 public class Point {
     public final double x;
-    public final double y;
+    public final double y; 
 
     public Point(double x, double y){
         this.x = x;
@@ -22,4 +22,31 @@ public class Point {
     public static double distance(Point p1, Point p2) {
 		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
 	}
+    //challenge
+    static double sumX = 0;
+    static double sumY = 0;
+    public static Point centerOfMass(Point[] points){
+        if(points.length == 0){
+            return null;
+        }
+        for(Point point : points){
+            sumX += point.x;
+            sumY += point.y;
+            System.out.println(sumX + sumY);
+        }
+        return new Point(sumX/points.length, sumY/points.length);
+    }
+
+    public double angle(){
+        if(x == 0 && y == 0){
+            return 0.0;
+        }
+        //found out a simple
+        double radianArc = Math.atan2(y , x);
+        double degree = Math.toDegrees(radianArc);
+        if(degree < 0){
+            degree += 360;
+        }
+        return degree;
+    }
 }
